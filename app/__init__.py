e02b0fd1bc2af30a1d55a60b852ee8703ad551e1
Wei Qiang <qwbu22@gmail.com>
2024-11-03 20:34:34 -0800

    finish wave2

--- app/__init__.py
+++ app/__init__.py
@@ -2,15 +2,21 @@ from flask import Flask
 from .db import db, migrate
 from .models import task
 from .routes.task_routes import tasks_bp
+import os
 
-def create_app():
+def create_app(config = None):
     app = Flask(__name__)
 
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:postgres@localhost:5432/task_list_api_development'
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('SQLALCHEMY_DATABASE_URI')
+    #'postgresql+psycopg2://postgres:postgres@localhost:5432/task_list_api_development'
 
+    if config:
+        app.config.update(config)
+    
     db.init_app(app)
     migrate.init_app(app, db)
+
     app.register_blueprint(tasks_bp)
 
     return app
--- app/routes/task_routes.py
+++ app/routes/task_routes.py
@@ -1,10 +1,119 @@
-from flask import Blueprint
+from flask import Blueprint, abort, make_response, request, Response
 from app.models.task import Task
 from ..db import db
+from sqlalchemy import asc,desc
 
 tasks_bp = Blueprint("tasks_bp", __name__, url_prefix="/tasks")
 
 @tasks_bp.post("")
 def create_task():
-    pass
+    request_body = request.get_json()
+    title = request_body.get("title")
+    description = request_body.get("description")
 
+    if not title or not description:
+        response = {
+            "details": "Invalid data"
+        }
+        abort(make_response(response , 400))
+
+    new_task = Task(title=title, description=description)
+    db.session.add(new_task)
+    db.session.commit()
+
+    response = {
+        "task": {
+            "id": new_task.id,
+            "title": new_task.title,
+            "description": new_task.description,
+            "is_complete":new_task.is_complete()
+        }
+    }
+    return response, 201    
+
+
+@tasks_bp.get("")
+def get_all_tasks():
+    query = db.select(Task)
+
+    sort_param = request.args.get("sort")
+    if sort_param == "asc":
+        query = query.order_by(asc(Task.title))
+    if sort_param == "desc":
+        query = query.order_by(desc(Task.title))
+    
+    else:
+
+        query = query.order_by(Task.id)
+
+    tasks = db.session.scalars(query)
+
+    tasks_response = []
+    for task in tasks:
+        tasks_response.append({
+            "id": task.id,
+            "title": task.title,
+            "description": task.description,
+            "is_complete": task.is_complete()
+        })
+
+    return tasks_response
+
+@tasks_bp.get("/<task_id>")
+def get_one_task_by_id(task_id):
+    task = validate_task(task_id)
+
+    return {
+        "task": {
+            "id": task.id,
+            "title": task.title,
+            "description": task.description,
+            "is_complete": task.is_complete()
+        }
+    }
+
+
+@tasks_bp.put("/<task_id>")
+def update_task(task_id):
+    task = validate_task(task_id)
+    request_body = request.get_json()
+
+    task.title = request_body["title"]
+    task.description = request_body["description"]
+    db.session.commit()
+
+    return {
+        "task": {
+            "id": task.id,
+            "title": task.title,
+            "description": task.description,
+            "is_complete": task.is_complete()
+        }
+    } 
+
+
+@tasks_bp.delete("/<task_id>")
+def delete_task(task_id):
+    task = validate_task(task_id)
+    db.session.delete(task)
+    db.session.commit()
+
+    return  {
+        "details": f'Task {task_id} "{task.title}" successfully deleted'
+    }
+
+def validate_task(task_id):
+    try:
+        task_id = int(task_id)
+    except:
+        response = {"message": f"Task {task_id} invalid"}
+        abort(make_response(response , 400))
+
+    query = db.select(Task).where(Task.id == task_id)
+    task = db.session.scalar(query)
+
+    if not task:
+        response = {"message": f"Task {task_id} not found"}
+        abort(make_response(response, 404))
+
+    return task
